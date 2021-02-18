cbbc505b4f8d4282fe3f8598b0a80f10289b63a7
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-02-18 17:12:47 +0100

    Fixed Indentation Code

--- 01_GAP.asm
+++ 01_GAP.asm
@@ -15,7 +15,7 @@ il controllo al programma chiamante senza che tali registri risultino alterati.
 ; ************ PROGRAMMA TEST ************
 
 .orig		x3000
-		LEA	R0, array	; in R0 <- indirizzo inizio array interi (/0)
+		LEA	R0, array			; in R0 <- indirizzo inizio array interi (/0)
 
 ; ************ SOTTOPROGRAMMA ************
 
@@ -29,15 +29,15 @@ il controllo al programma chiamante senza che tali registri risultino alterati.
 		
 			AND	R2,R2,#0		; inizializzo a zero registro MAX 
 			AND	R3,R3,#0		; inizializzo a zero registro min
-								; inizializzazioni necessarie in caso di 
-								; sequenza vuota -> altrim errore label fine
+							; inizializzazioni necessarie in caso di 
+							; sequenza vuota -> altrim errore label fine
 	
 			LDR	R1,R0,#0		; in R1 <- cont cella (prima) indirizzata da R0
 			BRZ	fine			; se zero -> sequenza terminata -> fine
 			ADD	R2,R1,#0		; in R2 (MAX) <- contenuto prima cella array
 			ADD	R3,R1,#0		; in R3 (min) <- contenuto prima cella array
 
-ciclo		ADD	R0,R0,#1		; incremento indirizzamento cella (in R0)
+ciclo		ADD	R0,R0,#1			; incremento indirizzamento cella (in R0)
 			LDR	R1,R0,#0		; leggo valore successivo array interi
 			BRZ	fine			; se zero -> sequenza terminata -> fine
 
@@ -45,18 +45,18 @@ ciclo		ADD	R0,R0,#1		; incremento indirizzamento cella (in R0)
 			ADD	R4,R4,#1		; in R4 <- Ca2 di R1 (alias -R1)
 		
 			ADD	R5,R2,R4		; in R5 <- risultato confronto "MAX-numero"
-			BRZP	val_min		; se nullo/positivo "Max > numero", quindi
-								; passare a valutare il minimo -> val_min
-								; altrimenti....					
+			BRZP	val_min			; se nullo/positivo "Max > numero", quindi
+							; passare a valutare il minimo -> val_min
+							; altrimenti....					
 			ADD	R2,R1,#0		; se negativo "numero > MAX" quindi newMAX
 		
-val_min		ADD	R5,R3,R4		; in R5 <- risultato confronto "min-numero"
-			BRNZ	ciclo		; se nullo/negativo "min > numero", quindi
-								; valutare numero successivo array -> ciclo
-								; altrimenti.....
+val_min		ADD	R5,R3,R4			; in R5 <- risultato confronto "min-numero"
+			BRNZ	ciclo			; se nullo/negativo "min > numero", quindi
+							; valutare numero successivo array -> ciclo
+							; altrimenti.....
 			ADD	R3,R1,#0		; se positivo "numero < min" quindi newmin
 		
-			BRNZP	ciclo		; si prosegue ciclo sino a fine sequenza (/0)
+			BRNZP	ciclo			; si prosegue ciclo sino a fine sequenza (/0)
 
 fine		NOT	R3,R3
 			ADD	R3,R3,#1		; Ca2 del min (-min)
@@ -68,15 +68,15 @@ fine		NOT	R3,R3
 			LD	R4, store4		; contenuto cella indirizzo store4 -> in R4
 			LD	R5, store5		; contenuto cella indirizzo store5 -> in R5		
 		
-; 			RET					; ritorno da sottoprogramma
+; 			RET				; ritorno da sottoprogramma
 
 ; ************ VAR / COST ****************
 	
-store1		.blkw	1			; riservo una cella memoria per contenuto R1
-store2		.blkw	1			; riservo una cella memoria per contenuto R2
-store3		.blkw	1			; riservo una cella memoria per contenuto R3
-store4		.blkw	1			; riservo una cella memoria per contenuto R4
-store5		.blkw	1			; riservo una cella memoria per contenuto R5
+store1		.blkw	1				; riservo una cella memoria per contenuto R1
+store2		.blkw	1				; riservo una cella memoria per contenuto R2
+store3		.blkw	1				; riservo una cella memoria per contenuto R3
+store4		.blkw	1				; riservo una cella memoria per contenuto R4
+store5		.blkw	1				; riservo una cella memoria per contenuto R5
 
 array		.fill	#112
 		.fill	#-27
