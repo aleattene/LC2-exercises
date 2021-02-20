94334947c20eef885b2da792db0b0a9d37d1b3a1
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-02-20 20:42:23 +0100

    Added comments row 8, RET & end instructions

--- 28_SEQMOD.asm
+++ 28_SEQMOD.asm
@@ -5,7 +5,7 @@
 
 ; ********** SOTTOPROGRAMMA ***********
 
-;SEQMOD		
+;SEQMOD					; nome sottoprogramma
 		ST	R1,store1	; scrivo contenuto R1 in cella di indirizzo store1
 		ST	R2,store2	; scrivo contenuto R2 in cella di indirizzo store2
 		ST	R3,store3	; scrivo contenuto R3 in cella di indirizzo store3
@@ -13,12 +13,12 @@
 		AND	R0,R0,R0	; verifico il valore presente in R0
 		BRNZ	out_zero	; se trovo N minore o uguale a zero vado a output zero
 
-; da qui in numero N è positivo
+; da qui in numero N Ã¨ positivo
 
-		ADD	R0,R0,#-2	; verifico se il numero è 1 (Brn) oppure 2 (Brz)
+		ADD	R0,R0,#-2	; verifico se il numero Ã¨ 1 (Brn) oppure 2 (Brz)
 		BRNZ	out_uno		; se trovo N uguale a 1 o 2 vado ad output_1
 		
-; da qui il numero N è maggiore di 2
+; da qui il numero N Ã¨ maggiore di 2
 		
 		AND	R1,R1,#0	; azzero il registro R1 (Sn-2)
 		AND	R2,R2,#0	; azzero il registro R2 (Sn-1)
@@ -33,7 +33,7 @@ ciclo		AND	R3,R3,#0	; azzero registro Sn
 
 		ADD	R3,R2,R1	; risultato Sn
 		ADD	R0,R0,-1	; riduco di uno il valore di R0
-		BRZ	out_n		; se ottengo zero la sequenza è terminata e vado ad emetterla in output
+		BRZ	out_n		; se ottengo zero la sequenza Ã¨ terminata e vado ad emetterla in output
 		ADD	R1,R2,#0	; Sn-1 diventa Sn-2
 		ADD	R2,R3,#0	; Sn diventa Sn-1
 		BRNZP	ciclo
@@ -42,7 +42,7 @@ val_ass		NOT 	R2,R2
 		ADD	R2,R2,#1	; imposto R2 come valore assoluto di (Sn-1)
 		ADD	R3,R2,R1	; calcolo Sn = |Sn-1|-2(Sn-2)
 		ADD	R0,R0,-1	; riduco di uno il valore di R0 (sequenza)
-		BRZ	out_n		; se ottengo zero la sequenza è terminata e vado ad emetterla in output
+		BRZ	out_n		; se ottengo zero la sequenza Ã¨ terminata e vado ad emetterla in output
 					; altrimenti
 		NOT 	R2,R2
 		ADD	R2,R2,#1	; ripristino il registro R2 da |Sn-1| a (Sn-1)
@@ -65,7 +65,7 @@ fine		LD	R1,store1	; porto in R1 il contenuto cella indirizzo store1
 		LD	R2,store2	; porto in R2 il contenuto cella indirizzo store2
 		LD	R3,store3	; porto in R2 il contenuto cella indirizzo store2
 
-;		RET
+;		RET			; ritorno da sottoprogramma
 
 ; ************ VARIABILI SOTTORPOGRAMMA e PROGRAMMA *************
 
@@ -78,9 +78,4 @@ store3		.blkw	1		; riservo una cella memoria per contenuto R3
 ;intero		.fill	#2
 intero		.fill	#6
 
-
-
-
-
-
-.end
+.end					; fine programma
