4229e4845c9f490bad447fe163d5c0e137df7134
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-02-22 11:47:01 +0100

    Added Descrizione Sottoprogramma

--- 29_CERCA_POS.asm
+++ 29_CERCA_POS.asm
@@ -1,3 +1,15 @@
+  ; ************ DESCRIZIONE SOTTOPROGRAMMA ************
+
+Il seguente sottoprogramma denominato CERCA_POS riceve:
+- nel registro R0 l’indirizzo della prima cella di una zona di memoria contenente in ciascuna cella un numero
+	a 16 bit in complemento a 2 (il valore 0 – zero – indica il termine della sequenza);
+- nel registro R1 un numero a 16 bit in complemento a 2.
+Il sottoprogramma inoltre, restituisce nel registro R0 la posizione del numero ricevuto nel registro R1 all'interno
+della sequenza (dove il primo numero della sequenza ha posizione 1, il secondo 2, ecc.). 
+Se il numero cercato non esiste nella sequenza, il sottoprogramma restituisce il valore 0.
+Nonostante l'utilizzo di altri registri della CPU, il sottoprogramma restituisce 
+il controllo al programma chiamante senza che tali registri risultino alterati.
+
 ; ********** PROGRAMMA TEST **********
 
 .orig		x3000
@@ -29,7 +41,7 @@ ciclo		LDR	R2,R0,#0	; in R2 valore della cella di indirizzo contenuto in R0
 					; quindi esco e riporto l'indice della sequenza
 		BRNP	ciclo		; altrimenti ripeto ciclo (scorro arrayM sino a term. zero)
 
-fine_zero	AND	R3,R3,#0	; azzero contatore R3 (perch� valore non trovato)
+fine_zero	AND	R3,R3,#0	; azzero contatore R3 (perchè valore non trovato)
 fine		ADD	R0,R3,#0	; contenuto R3 in R0 (come da specifica)
 
 		LD	R2,store2	; ripristino cont cella indir store2 in R2
